e9568d137ac6e5c886db42b2476150d7273305c7
Murilo Sartori <m.sartori22@gmail.com>
2021-02-09 21:48:39 -0300

    Resolução Execício 3 - semana 10

--- semana10/src/App.java
+++ semana10/src/App.java
@@ -1,5 +1,10 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, Mundo!");
+
+        Random aleatorio = new Random();
+        System.out.println(aleatorio.nextInt(2));
     }
 }
