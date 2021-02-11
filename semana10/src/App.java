e55615774895cc0072cb90c42da24959e78a2755
Murilo Sartori <m.sartori22@gmail.com>
2021-02-10 21:12:13 -0300

    Resolução Execício 5 - semana 10

--- semana10/src/App.java
+++ semana10/src/App.java
@@ -1,23 +1,32 @@
+import java.util.Locale;
+
 // import java.util.Random;
-import java.util.Calendar;
+// import java.util.Calendar;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        //Exercicio 2
         // System.out.println("Olá, Mundo!");
 
+        //Exercicio 3
         // Random aleatorio = new Random();
         // System.out.println(aleatorio.nextInt(2));
 
-        Calendar data = Calendar.getInstance();
-        int hora = data.get(Calendar.HOUR_OF_DAY);
-        int minuto = data.get(Calendar.MINUTE);
-        if(hora >= 0 && hora < 12){
-			System.out.printf("Bom Dia, no momento são %d:%d", hora, minuto);
-		}else if(hora > 12 && hora < 18){
-			System.out.printf("Boa Tarde, no momento são %d:%d", hora, minuto);
-		}else{
-            System.out.println(data);
-            System.out.printf("Boa Noite, no momento são %d:%d", hora, minuto);
-		}
+        //Exercicio 4
+        // Calendar data = Calendar.getInstance();
+        // int hora = data.get(Calendar.HOUR_OF_DAY);
+        // int minuto = data.get(Calendar.MINUTE);
+        // if(hora >= 0 && hora < 12){
+		// 	System.out.printf("Bom Dia, no momento são %d:%d", hora, minuto);
+		// }else if(hora > 12 && hora < 18){
+		// 	System.out.printf("Boa Tarde, no momento são %d:%d", hora, minuto);
+		// }else{
+        //     System.out.println(data);
+        //     System.out.printf("Boa Noite, no momento são %d:%d", hora, minuto);
+        // }
+        
+        //Exercicio 5
+        Locale local = Locale.getDefault();
+        System.out.println(local.getDisplayLanguage());
     }
 }
