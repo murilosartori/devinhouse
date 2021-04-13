e58f711b18293b7855f63b6106bb2ecad366c03d
Murilo Sartori <m.sartori22@gmail.com>
2021-04-12 21:20:34 -0300

    Resolução Exercício 11 - semana 15

--- semana14/src/exercicio07/Bidimensional.java
+++ semana14/src/exercicio07/Bidimensional.java
@@ -5,4 +5,9 @@ public class Bidimensional extends Forma {
 	public double getArea() {
 		return 0;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
 }
--- semana14/src/exercicio07/Tridimensional.java
+++ semana14/src/exercicio07/Tridimensional.java
@@ -2,4 +2,9 @@ package exercicio07;
 
 public abstract class Tridimensional extends Forma {
 	public abstract double getVolume();
+	
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
 }
